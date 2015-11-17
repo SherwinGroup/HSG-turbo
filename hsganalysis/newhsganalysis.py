095f9951ada6592ae722188f215a8f1008a7a157
h-banks <hbanks@gmail.com>
2015-11-16 16:44:35 -0800

    Changed neon line calculation

--- hsganalysis/newhsganalysis.py
+++ hsganalysis/newhsganalysis.py
@@ -194,7 +194,7 @@ class Absorbance(CCD):
 
 class NeonNoiseAnalysis(CCD):
     def __init__(self, fname, spectrometer_offset=None):
-        print 'opening', fname
+        #print 'opening', fname
         super(NeonNoiseAnalysis, self).__init__(fname, spectrometer_offset=spectrometer_offset)
 
         self.addenda = self.parameters['addenda']
@@ -221,7 +221,7 @@ class NeonNoiseAnalysis(CCD):
         #4, weak: 825.9
         #5, strong: 830.0
         """
-
+        print '\n\n'
 
         self.ccd_data = np.flipud(self.ccd_data)
         self.high_noise_region = np.array(self.ccd_data[30:230, :])
@@ -242,16 +242,25 @@ class NeonNoiseAnalysis(CCD):
         self.peak4 = np.array(self.ccd_data[1268:1288, :])
         self.peak5 = np.array(self.ccd_data[1381:1421, :])
 
-        self.signal1 = np.sum(self.peak1[:, 1])
-        self.error1 = np.sqrt(np.sum(self.peak1[:, 2]**2))
-        self.signal2 = np.sum(self.peak2[:, 1])
-        self.error2 = np.sqrt(np.sum(self.peak2[:, 2]**2))
-        self.signal3 = np.sum(self.peak3[:, 1])
-        self.error3 = np.sqrt(np.sum(self.peak3[:, 2]**2))
-        self.signal4 = np.sum(self.peak4[:, 1])
-        self.error4 = np.sqrt(np.sum(self.peak4[:, 2]**2))
-        self.signal5 = np.sum(self.peak5[:, 1])
-        self.error5 = np.sqrt(np.sum(self.peak5[:, 2]**2))
+        temp_max = np.argmax(self.peak1[:, 1])
+        self.signal1 = np.sum(self.peak1[temp_max - 1:temp_max + 2, 1])
+        self.error1 = np.sqrt(np.sum(self.peak1[temp_max - 1:temp_max + 2, 2]**2))
+
+        temp_max = np.argmax(self.peak2[:, 1])
+        self.signal2 = np.sum(self.peak2[temp_max - 1:temp_max + 2, 1])
+        self.error2 = np.sqrt(np.sum(self.peak2[temp_max - 1:temp_max + 2, 2]**2))
+
+        temp_max = np.argmax(self.peak3[:, 1])
+        self.signal3 = np.sum(self.peak3[temp_max - 1:temp_max + 2, 1])
+        self.error3 = np.sqrt(np.sum(self.peak3[temp_max - 1:temp_max + 2, 2]**2))
+
+        temp_max = np.argmax(self.peak4[:, 1])
+        self.signal4 = np.sum(self.peak4[temp_max - 1:temp_max + 2, 1])
+        self.error4 = np.sqrt(np.sum(self.peak4[temp_max - 1:temp_max + 2, 2]**2))
+
+        temp_max = np.argmax(self.peak5[:, 1])
+        self.signal5 = np.sum(self.peak5[temp_max - 1:temp_max + 2, 1])
+        self.error5 = np.sqrt(np.sum(self.peak5[temp_max - 1:temp_max + 2, 2]**2))
 
         self.signal_list = [self.signal1, self.signal2, self.signal3, self.signal4, self.signal5]
         self.error_list = [self.error1, self.error2, self.error3, self.error4, self.error5]
@@ -262,7 +271,9 @@ class NeonNoiseAnalysis(CCD):
         """
         This one puts high_noise, low_noise1, signal2, and error2 in a nice horizontal array
         """
-        self.results = np.array([self.high_noise, self.low_noise1, self.signal1, self.error1])
+        self.results = np.array([self.high_noise, self.low_noise1, self.signal5, self.error5])
+        # average = np.mean([self.low_noise1, self.low_noise2, self.low_noise3])
+        # self.results = np.array([self.high_noise, self.low_noise1, self.low_noise2, self.low_noise3, self.high_noise/average])
 
 def collect_noise(neon_list, param_name, folder_name, file_name, name='Signal'):
     """
@@ -270,7 +281,7 @@ def collect_noise(neon_list, param_name, folder_name, file_name, name='Signal'):
 
     param_name = string that we're gonna save!
     """
-    
+    #param_array = None
     for elem in neon_list:
         print "pname: {}".format(elem.parameters[param_name]),
         print "results:", elem.results
@@ -283,16 +294,20 @@ def collect_noise(neon_list, param_name, folder_name, file_name, name='Signal'):
     if len(param_array.shape) == 1:
         print "I don't think you want this file"
         return
-    # append the relative peak error
+   # append the relative peak error
+
+    print '\n', param_array, '\n'
+
     param_array = np.column_stack((param_array, param_array[:,4]/param_array[:,3] ))
     # append the snr
     param_array = np.column_stack((param_array, param_array[:,3]/param_array[:,2] ))
+
     try:
         param_array = param_array[param_array[:,0].argsort()]
     except:
         print "param_array shape", param_array.shape
         raise
-    
+
     try:
         os.mkdir(folder_name)
     except OSError, e:
@@ -304,9 +319,11 @@ def collect_noise(neon_list, param_name, folder_name, file_name, name='Signal'):
     file_name = file_name + '.txt'
 
     origin_import1 = param_name + ",Noise,Noise,Signal,error,rel peak error,peak signal-to-noise"
+    # origin_import1 = param_name + ",Noise,Noise,Noise,Noise,Ratio"
     origin_import2 = ",counts,counts,counts,counts,,"
+    # origin_import2 = ",counts,counts,counts,,"
     origin_import3 = ",High noise region,Low noise region,{},{} error,{} rel error, {}".format(name, name, name, name)
-
+    # origin_import3 = ",High noise region,Low noise region 1,Low noise region 2,Low noise region 3,High/low"
     header_total = origin_import1 + "\n" + origin_import2 + "\n" + origin_import3
 
     #print "Spec header: ", spec_header
